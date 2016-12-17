654a5d33c7401fc9e33441f8d19edaf2efe06210
Sanjeev K Shukla <animproin@gmail.com>
2016-12-18 04:37:55 +0530

    Upload initial code base.

---
+++ ui.apps/src/main/content/jcr_root/apps/my_aem_work/components/content/infoportlet/infoportlet.js
@@ -0,0 +1,8 @@
+use(function(){
+	title = granite.resource.properties["title"];
+	description = granite.resource.properties["description"];
+	return {
+		title: title,
+		description:description
+	};
+});
